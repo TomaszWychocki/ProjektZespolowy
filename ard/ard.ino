72b2dff81bbc3c4b950f1432ef3e5641cda0ff9d
Tomasz Wychocki <wychoniu@gmail.com>
2017-11-04 21:16:21 +0100

    Init

---
+++ ard/ard.ino
@@ -0,0 +1,111 @@
+#define TEMP1 A0 //Content temperature
+#define TEMP2 A1 //Heater temperature
+#define HEAT 8   //Heater relay
+
+String message;
+bool heater = false;
+bool action = false; //true - warming | false - wait
+double time = 0.0;
+double targetTemp = 0.0;
+double last = 0.0;
+
+void setup() {
+  Serial.setTimeout(20);
+  Serial.begin(9600);
+}
+
+double getContentTemperature() {
+  //....code
+  double t = 24.52;
+  return t;  
+}
+
+double getHeaterTemperature() {
+  //....code
+  double t = 41.52;
+  return t;  
+}
+
+void loop() {
+  while (Serial.available()) {
+    message = Serial.readStringUntil("\n");
+    if(message.startsWith("[getTEMP1]")) {
+      String resp = "TEMP1{" + String(getContentTemperature(),2) + "}";
+      Serial.println(resp);
+    }
+    else if(message.startsWith("[getTEMP2]")) {
+      String resp = "TEMP2{" + String(getHeaterTemperature(),2) + "}";
+      Serial.println(resp);
+    }
+    else if(message.startsWith("[getHEAT]")) {
+      String resp = "HEAT{" + String(heater) + "}";
+      Serial.println(resp);
+    }
+    else if(message.startsWith("[getTIME]")) {
+      String resp = "TIME{" + String(time,1) + "}";
+      Serial.println(resp);
+    }
+    else if(message.startsWith("[getACTION]")) {
+      String resp = "ACTION{" + String(action) + "}";
+      Serial.println(resp);
+    }
+    else if(message.startsWith("[setTEMP]")) { //[setTEMP]{60.3}
+      String val = message.substring(10, message.indexOf('}'));
+      targetTemp = val.toDouble();
+      Serial.println("OK");
+    }
+    else if(message.startsWith("[setTIME]")) { //[setTIME]{4569795}
+      String val = message.substring(10, message.indexOf('}'));
+      time = val.toDouble();
+      Serial.println("OK");
+    }
+    else if(message.startsWith("[setACTI]")) { //[setACTI]{1}
+      char val = message.charAt(10);
+      if(val == '1')
+        action = true;
+      else
+        action = false;
+      Serial.println("OK");
+    }
+    else {
+       Serial.println("ERR");
+    }
+  }
+
+  if(millis() - last >= 1000) {
+    last = millis();
+    time--;
+  }
+
+  if(time <= 0.0) {
+    time = 0.0;
+    heater = false;
+    if(targetTemp > 0) {
+      Serial.println("END");
+      targetTemp = -1.0;
+    }
+  }
+  else {
+    if(action) {
+      if(getContentTemperature() >= targetTemp) {
+        heater = false;
+      }
+      else {
+        if(getHeaterTemperature() >= targetTemp + 5.0) {
+          heater = false;
+        }
+        else {
+          heater = true;
+        }
+      }
+    }
+    else {
+      heater = false;
+    }
+  }
+
+  if(heater)
+    digitalWrite(HEAT, HIGH);
+  else
+    digitalWrite(HEAT, LOW);
+}
---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require('express');
+app = express();
+server = require('http').createServer(app);
+io = require('socket.io').listen(server);
+
+var SerialPort = require("serialport");
+var arduino = new SerialPort("/dev/ttyUSB0", {
+    baudRate : 9600,
+    dataBits : 8,
+    parity : 'none',
+    stopBits: 1,
+    flowControl : false,
+}); 
+
+var intervalObj = null;
+var intervalObj2 = null;
+var x = false;
+var lock = false;
+var commandBuffer = [];
+server.listen(8080);
+app.use(express.static('public'));
+
+arduino.on('open', function() {
+	arduino.on('data', function(data) {
+		var m = data.toString('ascii');
+		m = m.replace("\n", "");
+		console.log(m);
+
+		if(m.includes("TEMP1"))
+			io.sockets.emit('message', {type: "TEMP1", value: m});
+		else if(m.includes("TEMP2"))
+			io.sockets.emit('message', {type: "TEMP2", value: m});
+		else if(m.includes("HEAT"))
+			io.sockets.emit('message', {type: "HEAT", value: m});
+		else if(m.includes("TIME"))
+			io.sockets.emit('message', {type: "TIME", value: m});
+		else if(m.includes("ACTION"))
+			io.sockets.emit('message', {type: "ACTION", value: m});
+		else if(m.includes("END"))
+			io.sockets.emit('message', {type: "END", value: m});
+		else if(m.includes("ERR"))
+			console.log("COMMAND ERROR");
+		
+		lock = false;
+	});
+	
+	/*setInterval(function() {
+		console.log(".");	
+	}, 1000);*/
+});
+
+io.sockets.on('connection', function (socket) {
+	socket.on('start', function (data) {
+		if(data.value == '1') {
+			commandBuffer.push('[setTIME]{600.0}\n');
+			commandBuffer.push('[setACTI]{1}\n');
+			
+			if(intervalObj == null) {
+				intervalObj = setInterval(function() {
+					commandBuffer.push('[getTEMP1]\n');
+					commandBuffer.push('[getTEMP2]\n');
+					commandBuffer.push('[getHEAT]\n');
+					commandBuffer.push('[getTIME]\n');
+					commandBuffer.push('[getACTION]\n');		
+				}, 1000);
+			}
+			
+			if(intervalObj2 == null) {
+				intervalObj2 = setInterval(function() {
+					if(!lock && commandBuffer.length > 0){
+						lock = true;
+						var cmd = commandBuffer.pop();
+						arduino.write(cmd);
+					}
+				}, 100);
+			}
+			
+			x = true;
+			console.log("START");
+		}
+		else {
+			clearInterval(intervalObj);
+			clearInterval(intervalObj2);
+			intervalObj = null;
+			intervalObj2 = null;
+			while(commandBuffer.length > 0)
+				commandBuffer.pop();
+			x = false;
+			console.log("STOP");
+		}
+	});
+	
+	socket.on('send', function (data) {
+		commandBuffer.push('[setTEMP]{' + data.value + '}');
+	});
+	
+	io.sockets.emit('state', {value: x});
+});
+
+console.log("running");
